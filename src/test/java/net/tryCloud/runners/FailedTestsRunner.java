bb8510a861f11ad11ee688a42bf2734f58281451
Corsair <yran_r@ukr.net>
2022-01-11 22:55:12 -0600

    added folders with BrowserUtils

--- src/test/java/net/tryCloud/runners/FailedTestsRunner.java
+++ src/test/java/net/tryCloud/runners/FailedTestsRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "@target/rerun.txt",
-        glue = "com/cybertek/step_definitions"
+        glue = "net/tryCloud/step_definitions"
 )
 public class FailedTestsRunner {
 }
